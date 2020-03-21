90e171b430cfd666b955ce77b0b6ab2fbbce513a
Satinders <satinder.singh@essential.net.au>
2020-03-21 12:26:20 +1100

    progress

--- WebUI/ClientApp/src/components/PatientHome.js
+++ WebUI/ClientApp/src/components/PatientHome.js
@@ -1,4 +1,5 @@
 ﻿import React, { Component } from 'react';
+import Patient from './Patient';
 
 export class PatientHome extends Component {
     constructor(props) {
@@ -6,11 +7,17 @@ export class PatientHome extends Component {
         this.state = {
             name: '',
             email: '',
-            mobile: ''
+            mobile: '',
+            isLoaded: false,
+            patients: []
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        this.populatePatientList();
     }
 
     handleChange(event) {
@@ -22,28 +29,42 @@ export class PatientHome extends Component {
             name: this.state.name,
             email : this.state.email,
             mobile : this.state.mobile
-        };
-        
+        };        
         this.createPatient(newPatient);
+        this.setState({
+            name: '',
+            email: '',
+            mobile: ''
+        });
+
+        this.populatePatientList();
         event.preventDefault();
     }
         
 
     render() {
+        let contents = !this.state.isLoaded
+            ? <p><em>Loading...</em></p>
+            : <Patient patients={this.state.patients} />;
+
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <input type="text" name="name" placeholder="Patient Name" onChange={this.handleChange} required />
-                    <input type="text" name="email" placeholder="Email" onChange={this.handleChange} />
-                    <input type="text" name="mobile" placeholder="Mobile" onChange={this.handleChange} />
-                    <input type="submit" value="Submit" />
+                    <div className="form-group">
+                        <input type="text" name="name" placeholder="Patient Name" value={this.state.name} onChange={this.handleChange} required />
+                        <input type="text" name="email" placeholder="Email" value={this.state.email} onChange={this.handleChange} />
+                        <input type="text" name="mobile" placeholder="Mobile" value={this.state.mobile} onChange={this.handleChange} />
+                        <input type="submit" value="Submit" />
+                    </div>
                 </form>
+                <hr/>
+                <h3 id="tabelLabel" >Patient List</h3>
+                {contents}
             </div>
         );
     }
 
     createPatient(patient) {
-        console.log(JSON.stringify(patient));
         fetch('http://localhost:62177/api/patients', {
             method: 'POST',
             headers: {
@@ -51,6 +72,13 @@ export class PatientHome extends Component {
             },
             body: JSON.stringify(patient),
         });
+    }
+
+    async populatePatientList() {
+        const response = await fetch('http://localhost:62177/api/patients');
+        const data = await response.json();
+        this.setState({ patients: data, isLoaded: true });
+
     }
     
 }
