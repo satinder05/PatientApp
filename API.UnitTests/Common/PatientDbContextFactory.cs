a8067db1c552b2df3a0da61d841d09ba68758c0a
Satinders <satinder.singh@essential.net.au>
2020-03-22 12:45:19 +1100

    progress

---
+++ API.UnitTests/Common/PatientDbContextFactory.cs
@@ -0,0 +1,39 @@
+﻿using API.Infrastructure.Persistence;
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Text;
+using API.Model;
+
+namespace API.UnitTests.Common
+{
+    public class PatientDbContextFactory
+    {
+        public static PatientDbContext Create()
+        {
+            var options = new DbContextOptionsBuilder<PatientDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new PatientDbContext(options);
+            context.Database.EnsureCreated();
+
+            //Add Test data
+            //Patients
+            context.Patients.AddRange(new[] {
+                new Patient { Name = "John Smith", Email = "John.Smith@mail.com", Mobile = "+61410929102"},
+                new Patient { Name = "Test Patient", Email = "TestEmail@gmail.com", Mobile = "0400000000"},
+            });
+
+            context.SaveChanges();
+
+            return context;
+        }
+
+        public static void Destroy(PatientDbContext context)
+        {
+            context.Database.EnsureDeleted();
+
+            context.Dispose();
+        }
+    }
+}
